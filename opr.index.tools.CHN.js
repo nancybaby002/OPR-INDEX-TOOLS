5b810d4b716016ae5005d0ef5f9a79fc34517c41
nancybaby002 <260397380@qq.com>
2017-07-21 23:51:36 +0800

    修改版本号

--- opr.index.tools.CHN.js
+++ opr.index.tools.CHN.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OPR-INDEX-TOOLS
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       nancybaby233
 // @match        https://opr.ingress.com/*
