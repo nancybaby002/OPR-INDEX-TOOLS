f9609a56ee3b6fe2d2874ebec04ded8a0a792694
nancybaby002 <260397380@qq.com>
2017-07-22 08:57:05 +0800

    修改部分方法名，变量名

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # OPR-INDEX-TOOLS
 
-给OPR系统首页写的一个小插件。
-
+给OPR系统写的一个小插件。
+在原有基础上增加，总通过数，通过比例，开牌进度。
+需要使用tampermonkey
 
 ![示例图片](https://raw.githubusercontent.com/nancybaby002/OPR-INDEX-TOOLS/master/images/demo.png)
--- opr.index.tools.CHN.js
+++ opr.index.tools.CHN.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OPR-INDEX-TOOLS
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.1.20170722.1
 // @description  try to take over the world!
 // @author       nancybaby233
 // @match        https://opr.ingress.com/*
@@ -16,18 +16,24 @@
     const totalCount=parseInt($(".gold")[0].innerText);
     const createCount=parseInt($(".gold")[1].innerText);
     const rejectCount=parseInt($(".gold")[2].innerText);
-    const totalGet=createCount+rejectCount;
-    var rate=((rejectCount+createCount)/totalCount*100).toFixed(2);
-    $("#player_stats div").append('<br><p>'+
-        '<span class="glyphicon glyphicon-info-sign ingress-gray pull-left" uib-tooltip-trigger="outsideclick" uib-tooltip-placement="left" tooltip-class="goldBorder" uib-tooltip="通过比例"></span>'+
-        '<span style="margin-left: 5px" class="ingress-mid-blue pull-left">总通过数：</span>'+
-        '<span class="gold pull-right">'+totalGet+'</span>'+
-        '</p>');
-    $("#player_stats div").append('<br><p>'+
-        '<span class="glyphicon glyphicon-info-sign ingress-gray pull-left" uib-tooltip-trigger="outsideclick" uib-tooltip-placement="left" tooltip-class="goldBorder" uib-tooltip="通过比例"></span>'+
-        '<span style="margin-left: 5px" class="ingress-mid-blue pull-left">通过比例：</span>'+
-        '<span class="gold pull-right">'+rate+'%（'+totalGet+'/'+totalCount+'）</span>'+
-        '</p>');
+    const totalPass=createCount+rejectCount;
+    const passRatio=((rejectCount+createCount)/totalCount*100).toFixed(2);
+    // 总通过数
+    function showTotalPass(){
+        $("#player_stats div").append('<br><p>'+
+            '<span class="glyphicon glyphicon-info-sign ingress-gray pull-left" uib-tooltip-trigger="outsideclick" uib-tooltip-placement="left" tooltip-class="goldBorder" uib-tooltip="通过比例"></span>'+
+            '<span style="margin-left: 5px" class="ingress-mid-blue pull-left">总通过数：</span>'+
+            '<span class="gold pull-right">'+totalPass+'</span>'+
+            '</p>');
+    }
+    // 通过比例
+    function showPaasRatio(){
+        $("#player_stats div").append('<br><p>'+
+            '<span class="glyphicon glyphicon-info-sign ingress-gray pull-left" uib-tooltip-trigger="outsideclick" uib-tooltip-placement="left" tooltip-class="goldBorder" uib-tooltip="通过比例"></span>'+
+            '<span style="margin-left: 5px" class="ingress-mid-blue pull-left">通过比例：</span>'+
+            '<span class="gold pull-right">'+passRatio+'%（'+totalPass+'/'+totalCount+'）</span>'+
+            '</p>');
+    }
     //Bronze: 100
     //Silver: 750
     //Gold: 2500
@@ -38,25 +44,25 @@
         var barRate;
         var achieveName;
         var target;
-        if(totalGet<100){
+        if(totalPass<100){
             target=100;
-            barRate=totalGet/100*100;
+            barRate=totalPass/100*100;
             achieveName="铜";
-        }else if(totalGet>=100&&totalGet<750){
+        }else if(totalPass>=100&&totalPass<750){
             target=750;
-            barRate=totalGet/750*100;
+            barRate=totalPass/750*100;
             achieveName="银";
-        }else if(totalGet>=750&&totalGet<2500){
+        }else if(totalPass>=750&&totalPass<2500){
             target=2500;
-            barRate=totalGet/2500*100;
+            barRate=totalPass/2500*100;
             achieveName="金";
-        }else if(totalGet>=2500&&totalGet<5000){
+        }else if(totalPass>=2500&&totalPass<5000){
             target=5000;
-            barRate=totalGet/5000*100;
+            barRate=totalPass/5000*100;
             achieveName="钛";
-        }else if(totalGet>=5000&&totalGet<10000){
+        }else if(totalPass>=5000&&totalPass<10000){
             target=10000;
-            barRate=totalGet/10000*100;
+            barRate=totalPass/10000*100;
             achieveName="黑";
         }else{
             target=10000;
@@ -67,12 +73,13 @@
         var process='<br><p>'+
             '<span class="glyphicon glyphicon-info-sign ingress-gray pull-left" uib-tooltip-trigger="outsideclick" uib-tooltip-placement="left" tooltip-class="goldBorder" uib-tooltip="按你评审结果而驳回的Portal数量"></span>'+
             '<span style="margin-left: 5px" class="ingress-mid-blue pull-left">开'+achieveName+'牌进度：</span>'+
-            '<span class="gold pull-right">'+barRate+'%（'+totalGet+'/'+target+'）</span>'+
+            '<span class="gold pull-right">'+barRate+'%（'+totalPass+'/'+target+'）</span>'+
             '</p>';
         process=process+'<br><div class="progress progress-striped active"> <div class="progress-bar progress-bar-warning" role="progressbar"aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"style="width: '+barRate+'%;"> </div></div>';
         $("#player_stats div").append(process);
     }
+
+    showTotalPass();
+    showPaasRatio();
     showProcessBar();
-
-
 })();
